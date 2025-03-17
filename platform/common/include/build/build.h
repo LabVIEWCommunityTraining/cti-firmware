b78a993b18ddb762213b7e434c8d5ae20ab74116
Derrick Bommarito <negentropicdev@gmail.com>
2025-03-16 19:49:51 -0700

    Fix missing default I2C0 pins in resource availability field

--- platform/common/include/build/build.h
+++ platform/common/include/build/build.h
@@ -2,6 +2,6 @@
 #ifndef build_h_
 #define build_h_
 
-#define CTI_VER_TS 20250308T192352Z
+#define CTI_VER_TS 20250317T024655Z
 
 #endif //build_h_
--- platform/common/version.cmake
+++ platform/common/version.cmake
@@ -1,4 +1,4 @@
 #edit the firmware version here
 set(CTI_VER_MAJOR "0")
-set(CTI_VER_MINOR "5")
-set(CTI_VER_PATCH "4")
+set(CTI_VER_MINOR "9")
+set(CTI_VER_PATCH "1")
